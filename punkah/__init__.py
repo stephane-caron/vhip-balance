11210df6e76aeffffba89679a0f4b0b0b9302a34
Stéphane Caron <stephane.caron@lirmm.fr>
2017-12-15 18:34:34 +0100

    License switched to LGPL 3.0

--- punkah/__init__.py
+++ punkah/__init__.py
@@ -7,16 +7,17 @@
 # <https://github.com/stephane-caron/3d-balance>.
 #
 # 3d-balance is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
 # 3d-balance is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
 #
-# You should have received a copy of the GNU General Public License along with
-# 3d-balance. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License along
+# with 3d-balance. If not, see <http://www.gnu.org/licenses/>.
 
 from pendulum import InvertedPendulum
 from stabilize2d import Stabilizer2D
